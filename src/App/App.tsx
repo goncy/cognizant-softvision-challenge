32bc7a30003970586ee167d070d1e4e27795a497
goncy <gonzalo.pozzo4@gmail.com>
2021-08-26 09:59:56 -0300

    Initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import logo from "../assets/logo.png";
+
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <main>
+      <img alt="Softvision" src={logo} width={320} />
+      <h1 className={styles.title}>Lets get this party started</h1>
+    </main>
+  );
+}
+
+export default App;
